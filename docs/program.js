69bef70996af6672ab77b7e0def44496c5de9421
Francisco Baquero <175041860+LRfabaquero@users.noreply.github.com>
2025-09-13 12:53:30 -0500

    Creacion mapa base

---
+++ docs/program.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([4.695135420669119, -74.08618227939654], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
